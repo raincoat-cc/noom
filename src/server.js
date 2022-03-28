93becbfa3a5425e7d537d0bb86010db62e277f1a
WooSangWook <ww57403@naver.com>
2022-03-28 11:13:14 +0900

    2.8 Room Count part One

--- src/server.js
+++ src/server.js
@@ -18,9 +18,24 @@ const handleListen = () => console.log("Listening on http://localhost:3000");
 const httpServer = http.createServer(app);
 const wsServer = new Server(httpServer);
 
+function publicRooms() {
+  const {
+    sockets: {
+      adapter: { sids, rooms },
+    },
+  } = wsServer;
+  const publicRooms = [];
+  rooms.forEach((_, key) => {
+    if (sids.get(key) === "undefined") {
+      publicRooms.push(key);
+    }
+  });
+}
+
 wsServer.on("connection", socket => {
   socket["nickname"] = "Anon";
   socket.onAny(event => {
+    console.log(wsServer.sockets.adapter);
     console.log(`Socket Event: ${event}`);
   });
 
