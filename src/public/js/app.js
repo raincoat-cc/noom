e1c45d9e1f49e7668b89169fdf2bd66afb43c478
WooSangWook <ww57403@naver.com>
2022-04-21 17:25:48 +0900

    3.9 STUN

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -78,6 +78,13 @@ function handleCameraClick() {
 
 async function handleCameraChange() {
   await getMedia(camerasSelect.value);
+  const videoTrack = myStream.getVideoTracks()[0];
+  if (myPeerConnection) {
+    const videoSender = myPeerConnection
+      .getSenders()
+      .find(sender => sender.track.kind === "video");
+    videoSender.replaceTrack(videoTrack);
+  }
 }
 
 muteBtn.addEventListener("click", handleMuteClick);
